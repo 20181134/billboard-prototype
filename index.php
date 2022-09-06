81a9859727cce3c8dd381810e71571e51a2c6652
takeguchikento <20181134@mita-is.ed.jp>
2022-09-06 23:18:12 +0900

    development

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>Prototype</title>
         <link rel="stylesheet" href="stylesheet.css">
+        <?php
+        session_start();
+        ?>
     </head>
     <body>
         <header>
@@ -13,8 +16,15 @@
                 <a class="headerlinks" href="">Private Messages</a>
             </div>
             <div class="search">
-                Signed in as <a href="">Example</a>
-                <input type="text" placeholder="Search">
+                <?php
+                if (isset($_SESSION['user'])) {
+                    echo 'Signed in as <a href="', $_SESSION['user']['profilelink'], '">', $_SESSION['user']['username'], '</a>';
+                } else {
+                    echo '<a href="./signin.php">Sign In</a>';
+                }
+                ?>
+                <!-- Signed in as <a href="">Example</a> -->
+                <input type="text" name="search" placeholder="Search">
                 <input type="submit" value="Search">
             </div>
         </header>
@@ -23,12 +33,34 @@
                 <div class="information">
                     <p class="titletext">Your Account</p>
                     <div class="account-info">
+                        <?php
+                        if (isset($SESSION_['user'])) {
+                            echo '<img src="', $_SESSION['user']['profilepic'], '" class="prof">';
+                            echo '<p class="user">', $_SESSION['user']['username'], '</p>';
+                        } else {
+                            echo '<a href="./signin.php">Sign In</a> or <a href="createaccount.php">Create a new account</a> and tweet now!';
+                        }
+                        ?>
+                        <!--
                         <img src="./img25.jpg" class="prof">
                         <p class="user">Example</p>
+                        -->
                     </div>
                     <div class="moreinfo">
+                        <?php
+                        if (isset($_SESSION['user'])) {
+                            $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8;', 'admin', 'password');
+                            $sql = 'SELECT COUNT(*) as cnt FROM tweet';
+                            $rs = mysql_query($sql);
+                            $row = mysql_fetch_assoc($rs);
+                            $count = $row['cnt'];
+                            echo '<p class="desc">Tweets: ', $count;
+                        }
+                        ?>
+                        <!-- 
                         <p class="desc">Tweets: 5</p>
                         <p class="desc">Bio: Hello World</p>
+                        -->
                     </div>
                 </div>
                 <div class="tl">
