47cec6d0505d95e26722d957b08a8c825454e3c9
takeguchikento <20181134@mita-is.ed.jp>
2022-09-12 09:30:02 +0900

    development

--- account-output.php
+++ account-output.php
@@ -9,19 +9,34 @@
         session_start();
         unset ($_SESSION['user']);
         $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8', 'admin', 'password');
-        $stmt = $pdo->prepare('INSERT INTO userdata values(?, ?, ?, ?)');
+        $stmt = $pdo->prepare('INSERT INTO userdata values(?, ?, ?, ?, ?)');
         // profilepageの値は現在nullに設定　createaccount.phpにアバター欄を作成する - Done
-        // ここにアバターの写真をディレクトリに移す機能を設置
-        
-        $stmt->execute(null, $_REQUEST['username'], $_REQUEST['password'], null);
-        $sql = $pdo->prepare('SELECT * FROM userdata where userid=?');
-        $sql->execute($_REQUEST['username']);
-        foreach ($sql as $row) {
-            $_SESSION['user']=[
-                'username'=>$row['username'],
-                'profilepic'=>$row['profilepic'],
-                'id'=>$row['userid']
-            ];
+        // ここにアバターの写真をディレクトリに移す機能を設置 - done
+        if (is_uploaded_file['avatar']['tmp_name']) {
+            if (!file_exists('avatar')) {
+                mkdir('avatar');
+            }
+            $file = 'avatar/'.basename($_FILES['avatar']['tmp_name']);
+            if (move_uploaded_file($_FILES['avatar']['tmp_name'], $file)) {
+                // 個人ページを作成
+                $stmt->execute(null, $_REQUEST['username'], $_REQUEST['password'], $file, null);
+            $sql = $pdo->prepare('SELECT * FROM userdata where username=? and password=?');
+            $sql->execute($_REQUEST['username'], $_REQUEST['password']);
+            foreach ($sql as $row) {
+                $_SESSION['user']=[
+                    'username'=>$row['username'],
+                    'profilepic'=>$row['profilepic'],
+                    'id'=>$row['userid'],
+                    'profilelink'=>$row['profilepage']
+                ];
+            }
+                header('Location: ./index.php');
+                exit;
+            } else {
+                echo 'Something went wrong';
+            }
+        } else {
+            echo 'Select an avatar';
         }
         ?>
     </body>
