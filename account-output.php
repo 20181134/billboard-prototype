7c814691393aeee62282b3ab26c5a2db8836c3e9
takeguchikento <20181134@mita-is.ed.jp>
2022-09-26 08:35:59 +0900

    Test

--- account-output.php
+++ account-output.php
@@ -8,53 +8,56 @@
         <?php
         session_start();
         unset ($_SESSION['user']);
-        // profilepageの値は現在nullに設定　createaccount.phpにアバター欄を作成する - Done
-        // ここにアバターの写真をディレクトリに移す機能を設置 - done
-        if (is_uploaded_file['avatar']['tmp_name']) {
-            if (!file_exists('avatar')) {
-                mkdir('avatar');
-            }
-            $file = 'avatar/'.basename($_FILES['avatar']['tmp_name']).'.png';
-            if (move_uploaded_file($_FILES['avatar']['tmp_name'], $file)) {
-                // 個人ページを作成 profilepageの値は現在nullに設定
-                $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8', 'admin', 'password');
-                $stmt = $pdo->prepare('INSERT INTO userdata values(?, ?, ?, ?, ?)');
-                if ($stmt->execute([null, $_REQUEST['username'], $_REQUEST['password'], $file, null])) {
-                echo 'データを追加';
-                } else {
-                    print_r ($stmt -> errorInfo());
-                }
-                $sql = $pdo->prepare('SELECT * FROM userdata where username=? and password=?');
-                if ($sql->execute([$_REQUEST['username'], $_REQUEST['password']])) {
-                    foreach ($sql as $row) {
-                        $_SESSION['user']=[
-                            'username'=>$row['username'],
-                            'profilepic'=>$row['avatar'],
-                            'id'=>$row['userid'],
-                            'profilelink'=>$row['profilepage']
-                        ];
-                    }
-                    echo 'SQL success!';
-                    // ここからテスト
-                    if (!file_exists('user')) {
-                        mkdir('user');
-                    }
-                    $user = file_get_contents('./profile.txt');
-                    $user = str_replace('insert_avatar_here', '../'.$_SESSION['user']['profilepic'], $user);
-                    $user = str_replace('insert_username_here', $_SESSION['user']['username'], $user);
-                    $user = str_replace('insert_userid_here', $_SESSION['user']['id'], $user);
-                    file_put_contents('./user/'.$_SESSION['user']['username'].'.php', $user);
-                    // ここまで
-                } else {
-                    print_r ($sql -> errorInfo());
-                }
-                header('Location: ./index.php');
-                exit();
+        $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8', 'admin', 'password');
+        // ここからテスト
+        $check = $pdo->prepare('SELECT COUNT(*) AS ck FROM userdata where username=?');
+        if ($check->execute([$_REQUEST['username']])) {
+            if ($row['ck'] != 0) {
+                echo 'Username has already taken'; 
             } else {
-                echo 'Something went wrong';
+                if (is_uploaded_file['avatar']['tmp_name']) {
+                    if (!file_exists('avatar')) {
+                    mkdir('avatar');
+                }
+                $file = 'avatar/'.basename($_FILES['avatar']['tmp_name']).'.png';
+                if (move_uploaded_file($_FILES['avatar']['tmp_name'], $file)) {
+                    $stmt = $pdo->prepare('INSERT INTO userdata values(?, ?, ?, ?, ?)');
+                    if ($stmt->execute([null, $_REQUEST['username'], $_REQUEST['password'], $file, null])) {
+                        echo 'データを追加';
+                    } else {
+                        print_r ($stmt -> errorInfo());
+                    }
+                    $sql = $pdo->prepare('SELECT * FROM userdata where username=? and password=?');
+                    if ($sql->execute([$_REQUEST['username'], $_REQUEST['password']])) {
+                        foreach ($sql as $row) {
+                            $_SESSION['user']=[
+                                'username'=>$row['username'],
+                                'profilepic'=>$row['avatar'],
+                                'id'=>$row['userid'],
+                                'profilelink'=>$row['profilepage']
+                            ];
+                        }
+                        echo 'SQL success!';
+                        if (!file_exists('user')) {
+                            mkdir('user');
+                        }
+                        $user = file_get_contents('./profile.txt');
+                        $user = str_replace('insert_avatar_here', '../'.$_SESSION['user']['profilepic'], $user);
+                        $user = str_replace('insert_username_here', $_SESSION['user']['username'], $user);
+                        $user = str_replace('insert_userid_here', $_SESSION['user']['id'], $user);
+                        file_put_contents('./user/'.$_SESSION['user']['username'].'.php', $user);
+                    } else {
+                        print_r ($sql -> errorInfo());
+                    }
+                    header('Location: ./index.php');
+                    exit();
+                } else {
+                    echo 'Something went wrong';
+                }
+            } else {
+                echo 'Select an avatar';
             }
-        } else {
-            echo 'Select an avatar';
+            }
         }
         ?>
     </body>
---
+++ user/test10.php
@@ -0,0 +1,117 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="../stylesheet.css">
+    </head>
+    <body>
+    <header>
+            <div class="logo">
+                <h2>Prototype</h2>
+                <a class="headerlinks" href="../index.php">Home</a>
+                <a class="headerlinks" href="../profile.php">Profile</a>
+                <a class="headerlinks" href="">Private Messages</a>
+            </div>
+            <div class="search">
+                <div class="breakwater1">
+                <?php
+                    session_start();
+                    if (isset($_SESSION['user'])) {
+                        echo 'Signed in as <a href="../signout.php">', $_SESSION['user']['username'], '</a>';
+                    } else {
+                        echo '<a href="../signin.php">Sign In</a>';
+                    }
+                ?>
+                </div>
+                <!-- Signed in as <a href="">Example</a> -->
+                <form action="search-output.php" method="post" class="headersearch">
+                    <input type="text" name="search" placeholder="Search">
+                    <input type="submit" value="Search">
+                </form>
+            </div>
+        </header>
+        <main>
+            <div class="contents">
+                <div class="information">
+                    <p class="titletext">Account Info</p>
+                    <div class="account-info">
+                        <img src="../avatar/phpUK9lFf.png" class="prof">
+                        <p class="user">test10</p>
+                    </div>
+                    <div class="moreinfo">
+                        <!--
+                        <p class="desc">Bio: Hello</p>
+                        -->
+                    </div>
+                </div>
+                <div class="tl">
+                    <div class="timeline">
+                        <!--
+                            ここから追加するデータ
+                        <div class="tweet">
+                            <img src="avatar1.jpg" class="avatar1">
+                            <div class="cont">
+                                <b class="username">Example2</b>
+                                <p class="contents1">Hello, World!</p>
+                                <p class="time">13:48:38 9/6/2022</p>
+                            </div>
+                        </div>
+                        <hr class="division">
+                        -->
+                        <!--
+                        <div class="tweet">
+                            <img src="avatar1.jpg" class="avatar1">
+                            <div class="cont">
+                                <b class="username">Example2</b>
+                                <p class="contents1">Hello, World!</p>
+                                <p class="time">13:48:38 9/6/2022</p>
+                            </div>
+                        </div>
+                        <hr class="division">
+                        -->
+                        <?php
+                        $userid = '19';
+                        /*
+                        $path = __FILE__;
+                        //echo basenmae($path);
+                        $path2 = basename($path);
+                        $path2 = str_replace('.php', '', $path2);
+                        $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8', 'admin', 'password');
+                        $stmt = $pdo->prepare('SELECT * FROM tweets where uploader=?');
+                        if ($stmt->execute([$path2])) {
+                            foreach ($stmt as $row) {
+                                echo '<div class="tweet">';
+                                echo '<img class="avatar1" src="../', $row['avatar'], '">';
+                                echo '<div class="cont">';
+                                echo '<b class="username">', $row['uploader'], '</b>';
+                                echo '<p class="contents1">', $row['contents'], '</p>';
+                                echo '<p class="time">', $row['time'], '</p>';
+                                echo '</div>';
+                                echo '</div>';
+                                echo '<hr class="division">';
+                            }
+                        } else {
+                            print_r ($stmt -> errorInfo());
+                        }*/
+                        $pdo = new PDO('mysql:host=localhost;dbname=tweet;charset=utf8', 'admin', 'password');
+                        $stmt = $pdo->prepare('SELECT * FROM tweets WHERE userid = ?');
+                        if ($stmt->execute([$userid])) {
+                            foreach ($stmt as $row) {
+                                echo '<div class="tweet">';
+                                echo '<img class="avatar1" src="../', $row['avatar'], '">';
+                                echo '<div class="cont">';
+                                echo '<b class="username">', $row['uploader'], '</b>';
+                                echo '<p class="contents1">', $row['contents'], '</p>';
+                                echo '<p class="time">', $row['time'], '</p>';
+                                echo '</div>';
+                                echo '</div>';
+                                echo '<hr class="division">';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
